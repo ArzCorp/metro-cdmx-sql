48d44953fc35172b2a7e299b48c2889f9cc86f4f
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-04-05 07:56:38 -0600

    ✨ add query to insert data

--- CREATE-TABLE-TRAINS.sql
+++ CREATE-TABLE-TRAINS.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `subway_trains` (
-  `serial_number` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `serial_number` VARCHAR(10) NOT NULL,
   `name` VARCHAR(255),
   `subway_line_id` BIGINT(20) UNSIGNED NOT NULL,
   `type` TINYINT(4) NOT NULL,
   `year` INT(4) NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`serial_number`),
   CONSTRAINT `trains_line_id_fk`
   FOREIGN KEY (`subway_line_id`) REFERENCES `subway_lines` (`id`)
 )
---
+++ INSERT-TYPES-TRAIN.sql
@@ -0,0 +1,3 @@
+INSERT INTO types_train (name) VALUES
+  ('Neumático'),
+  ('Férreo');
